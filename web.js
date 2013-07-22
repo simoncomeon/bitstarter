d269b748d29059daf95aa5633f6a82bfdc44e257
Ubuntu <ubuntu@ip-172-31-27-166.us-west-2.compute.internal>
2013-07-22 09:49:25 +0000

    web.js updated to 8080

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var fs =require('fs');
+var htmlFile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   var mybuff = new Buffer("index","utf-8");
-   mybuff = fs.readFileSync('index.html'); 
-  var output = mybuff.toString('utf-8');
-  response.send(output);
+ var html =fs.readFileSync(htmlFile).toString();
+ //  var mybuff = new Buffer("index","utf-8");
+ //  mybuff = fs.readFileSync('index.html'); 
+//  var output = mybuff.toString('utf-8');
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
